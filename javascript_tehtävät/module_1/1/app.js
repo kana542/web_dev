e417073406209e10abaeee9e2fc85e9f4102728d
unknown <jonne.timonen@metropolia.fi>
2024-04-07 17:42:18 +0300

    done

--- javascript_tehtävät/module_1/1/app.js
+++ javascript_tehtävät/module_1/1/app.js
@@ -92,5 +92,24 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 // 10.
+let dices = parseInt(prompt("Anna noppien määrä: "));
+let sum = parseInt(prompt("Anna heittojen summa: "));
+
+if (sum > (dices * 6)) {
+    document.querySelector('#target').innerHTML = `The sum is not possible`;
+} else {
+    let prob = 0;
+    for (let i = 0; i < 10000; i++) {
+        let round = 0;
+        for (let t = 0; t < dices; t++) {
+            round += Math.floor(Math.random() * 6) + 1;
+        }
+        if (round === sum) {
+            prob++;
+        }
+    }
+    let calc = prob / 10000;
+    document.querySelector('#target').innerHTML = `Probability to get sum ${sum} with ${dices} dice is ${(calc * 100).toFixed(0)}%`;
+}
 
 });
